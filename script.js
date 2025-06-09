34e52ea0af3d98dbb8664f868eac859c9c7b5665
geovanamelato <geovanamelato2006@gmail.com>
2025-06-09 10:36:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-console.log("Site Educacional IF Técnico carregado!");
 
-
-const perguntas = [
-    "Qual é a capital do Brasil?",
-    "Quanto é 5 x 6?",
-    "Quem escreveu 'Dom Casmurro'?",
-    "O que é HTML?",
-    "Qual a fórmula da água?"
-  ];
-  
-  function proximaPergunta() {
-    const pergunta = perguntas[Math.floor(Math.random() * perguntas.length)];
-    document.getElementById("pergunta").innerText = pergunta;
-  }
-  
